9b84102918b5dc024a276c5995474f6846168ac9
miguelangelo78 <santosmiguel25@gmail.com>
2014-10-09 19:35:24 +0100

    Compile 13.0 - Expression pattern fixed to ungreedy

--- src/com/core/qassembler/constants/QConstants.java
+++ src/com/core/qassembler/constants/QConstants.java
@@ -35,7 +35,7 @@ public interface QConstants {
 	final String PATT_EXPRESSION_VAR="((?:[0-9]|\\$.+?)+?)(?:[ |	]+?)?([+\\-\\*\\/])+?(?:[ |	]+?)?((?:[0-9]|\\$.+?)+?$)";
 	final String PATT_EXPRESSION="([0-9]+?)(?:[ |	]+?)?([+\\-\\*\\/])+?(?:[ |	]+?)?([0-9]+?$)";
 	final String PATT_EXPRESSION_V2="[^a-z|0-9](?:[ |	|\\(]+)?[0-9]+?(?:[ |	]+?)?(?:[\\(\\)+\\-\\*\\/]+?(?:[ |	]+?)?[0-9])+(?:[ |	|\\)]+)?";
-	final String PATT_EXPRESSION_V3="\\(+?(?:(?:[0-9])?.+)?\\)";
+	final String PATT_EXPRESSION_V3="\\(+?(?:(?:[0-9])?.+?)?\\)";
 	final String PATT_VARIABLEDECL="^(?:[ |	]+?)?\\$.+?,.+?$";
 	final String PATT_VARIABLENAME="^(?:[ |	]+?)?\\$(?:[ |	]+?)?(.+?)$";
 	final String PATT_STRINGCONSTANT="mov.+?\\[.+?\\].+?\"(.+?)\"(?:.+?)?$";
